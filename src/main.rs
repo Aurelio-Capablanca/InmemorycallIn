d42808708d0bb9f2a179a1e6d78fe2e7fc53651e
Aurelio <aurelio.iempereur@gmail.com>
2025-06-13 20:44:40 -0600

    changes for console behavior

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct Person {
 
 impl Person {
     fn new() -> Self {
-        println!("Started New Person!!!");
+        //println!("Started New Person!!!");
         Person {
             name: String::new(),
             lastname: String::new(),
@@ -35,7 +35,7 @@ impl Person {
     fn get_lastname(&self) -> &str {
         &self.lastname
     }
-    
+
     fn get_email(&self) -> &str {
         &self.email
     }
@@ -46,43 +46,56 @@ struct RepositoryPeople {
 }
 
 impl RepositoryPeople {
-    fn new() -> Self{
-        RepositoryPeople{
-            persons: Vec::new()
+    fn new() -> Self {
+        RepositoryPeople {
+            persons: Vec::new(),
         }
     }
-    
-    fn add_person(&mut self, person: Person){
+
+    fn add_person(&mut self, person: Person) {
         self.persons.push(person);
     }
+}
+
+fn call_inserts(input: &mut String) -> Person {
+    let mut person = Person::new();
+    println!("Enter Your Name ! ");
+    io::stdin().read_line(input).unwrap();
+    person.set_name(input.trim().to_string());
+    input.clear();
+
+    println!("Enter Your Last Name ! ");
+    io::stdin().read_line(input).unwrap();
+    person.set_lastname(input.trim().to_string());
+    input.clear();
+
+    println!("Enter Your Email ! ");
+    io::stdin().read_line(input).unwrap();
+    person.set_email(input.trim().to_string());
+    input.clear();
+    person
 }
 
 fn main() {
     println!("TESTING!");
     let mut repository = RepositoryPeople::new();
-    let mut person = Person::new();
     let mut input = String::new();
+    let mut flag: bool = true;
+    while flag {
+        let person = call_inserts(&mut input); 
+        repository.add_person(person);   
+        println!("Do you want to continue? y/n");
+        io::stdin().read_line(&mut input).unwrap();
+        println!("Entered:  {}",input);
+        if input.trim().eq_ignore_ascii_case("n") {
+            flag = false;
+        }
+        input.clear();
+    }
     
-    println!("Enter Your Name ! ");
-    io::stdin().read_line(&mut input).unwrap();
-    person.set_name(input.trim().to_string());
-    input.clear();
-
-    println!("Enter Your Last Name ! ");
-    io::stdin().read_line(&mut input).unwrap();
-    person.set_lastname(input.trim().to_string());
-    input.clear();
-
-    println!("Enter Your Email ! ");
-    io::stdin().read_line(&mut input).unwrap();
-    person.set_email(input.trim().to_string());
-    input.clear();
-    
-    repository.add_person(person);
-
-    for person in &repository.persons  {
+    for person in &repository.persons {
         println!(
-            "name {}, lastname {}, email {}",
+            "name: {}, lastname: {}, email: {}",
             person.get_name(),
             person.get_lastname(),
             person.get_email()
